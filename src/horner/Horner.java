bcdb28bdb2536ce82ed6258bcb109140cf91427b
Jakub Fałowski <kubafa99@gmail.com>
2021-03-28 01:29:34 +0100

    poprawione 28.03

--- src/horner/Horner.java
+++ src/horner/Horner.java
@@ -6,6 +6,10 @@ public class Horner {
 
     public static void main(String[] args) {
         
+        Scanner wybor = new Scanner(System.in);
+        System.out.println("Wybierz program \n 0 - Obliczanie wartości w punkcie \n 1 - Dzielenie dwumianu  ");
+        int l_wyboru = wybor.nextInt();
+            
         int stopienpoczatkowy = 0;
         while(stopienpoczatkowy < 2){
             Scanner scan = new Scanner(System.in);
@@ -14,11 +18,6 @@ public class Horner {
         }
         int stopien = stopienpoczatkowy;
         
-        int dziel = 0;
-        Scanner scan2 = new Scanner(System.in);
-        System.out.println("\nPodaj współczynnik dwumianu ");
-        dziel = scan2.nextInt();
-        
         int[] wspolczynnik = new int[stopien+1];
         int[] wynik = new int[stopien+2];
         
@@ -26,15 +25,12 @@ public class Horner {
             Scanner scan3 = new Scanner(System.in);
             System.out.println("\nPodaj liczbę ... x^"+stopien);
             wspolczynnik[stopien] = scan3.nextInt();       
-            wynik[stopien] = wynik[stopien+1]*dziel+wspolczynnik[stopien];
-            System.out.println("Reszta: "+wynik[stopien]);
             stopien--;
         }
-        
-        a(stopienpoczatkowy,wspolczynnik,wynik,stopien);
-        b(stopienpoczatkowy,dziel,wspolczynnik,wynik,stopien);
+        if(l_wyboru == 0) wartosc_w_punkcie(stopienpoczatkowy,wspolczynnik,wynik,stopien);
+        if(l_wyboru == 1) dzielenie_dwumianu(stopienpoczatkowy,wspolczynnik,wynik,stopien);
     }
-    public static void a(int stopienpoczatkowy,int[] wspolczynnik, int[] wynik, int stopien){
+    public static void wartosc_w_punkcie(int stopienpoczatkowy,int[] wspolczynnik, int[] wynik, int stopien){
         
         Scanner scan3 = new Scanner(System.in);
         System.out.println("\nPodaj punkt p ");
@@ -48,19 +44,29 @@ public class Horner {
         }
         System.out.println("W("+p+") = "+suma);
     }
-    public static void b(int stopienpoczatkowy,int dziel, int[] wspolczynnik, int[] wynik, int stopien){
+    public static void dzielenie_dwumianu(int stopienpoczatkowy, int[] wspolczynnik, int[] wynik, int stopien){
+        int dziel = 0;
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("\nPodaj współczynnik b dwumianu ");
+        dziel = scan2.nextInt();
         
         while(dziel == 0){
-            Scanner scan2 = new Scanner(System.in);
+            Scanner scan3 = new Scanner(System.in);
             System.out.println("Podaj dzielnik wielomianu ");
-            dziel = scan2.nextInt();
+            dziel = scan3.nextInt();
         }
-        System.out.println();
-        System.out.print("W(x)=(x-"+dziel+")*");
         
-        while(stopienpoczatkowy > 0){
-            System.out.print("("+wynik[stopienpoczatkowy]+"x^"+(stopienpoczatkowy-1)+")+ ");
-            stopienpoczatkowy--;
+        System.out.print("W(x)=(x-"+dziel+")*");
+        while(stopienpoczatkowy >= 0){
+            if(stopienpoczatkowy > 0){
+                wynik[stopienpoczatkowy] = wynik[stopienpoczatkowy+1]*dziel+wspolczynnik[stopienpoczatkowy];
+                System.out.print("("+wynik[stopienpoczatkowy]+"x^"+(stopienpoczatkowy-1)+")+ ");
+            }
+            else{
+                int reszta = dziel*wynik[1]+wspolczynnik[0];
+                System.out.println("Reszta: "+reszta);
+            }
+                stopienpoczatkowy--;
         }
     }
 }
